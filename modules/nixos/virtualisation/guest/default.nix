0b42745190c18bf631acae8c682e1fff46039484
Fabian Sabau <fabian.sabau+git@gmail.com>
2025-03-15 00:28:32 +0100

    feat: add nixos module for enabling virtualisation guests

---
+++ modules/nixos/virtualisation/guest/default.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  virtual ? false,
+  namespace,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.${namespace}.virtualisation.guest;
+in
+{
+  options.${namespace}.virtualisation.guest = {
+    enable = mkOption {
+      type = types.bool;
+      default = virtual;
+      description = "Whether to enable guest services for virtual machines.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Enable QEMU guest agent when in a QEMU/KVM VM
+    services.qemuGuest.enable = true;
+
+    # Enable VirtualBox guest additions when in a VBox VM
+    virtualisation.virtualbox.guest.enable = true;
+
+    # TODO add all users to vboxsf group
+
+    # Additional helpful settings for VMs
+    services.spice-vdagentd.enable = mkIf config.services.xserver.enable true;
+  };
+}
