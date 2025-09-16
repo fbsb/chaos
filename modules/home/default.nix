788a951906392a101ecc76b355498a4b009e9ff3
Fabian Sabau <git@fbsb.dev>
2025-09-16 20:06:54 +0200

    feat: add junction app chooser

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -12,6 +12,7 @@ in
       apps.ghostty.enable = true;
       apps.zen-browser.enable = true;
       apps.vscode.enable = true;
+      apps.junction.enable = true;
       cli.git.enable = true;
       cli.zsh.enable = true;
       crypto.gpg.enable = true;
