391a8ded6a5fa338781fc083f115961f538412a2
Fabian Sabau <git@fbsb.dev>
2025-11-15 13:58:09 +0100

    fix: enable nvidia power management for hibernation

--- modules/nixos/chaos/hardware/nvidia.nix
+++ modules/nixos/chaos/hardware/nvidia.nix
@@ -29,7 +29,7 @@ in
 
     hardware.nvidia = {
       modesetting.enable = true;
-      powerManagement.enable = false;
+      powerManagement.enable = true;
       powerManagement.finegrained = false;
       open = true;
       nvidiaSettings = true;
