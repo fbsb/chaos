e442c32feef370134ed1988eba507bba00d75032
Fabian Sabau <git@fbsb.dev>
2026-01-21 00:00:56 +0100

    feat: add wifi quirks configuration for regulatory domain issues

--- modules/aspects/chaos/hardware/framework.nix
+++ modules/aspects/chaos/hardware/framework.nix
@@ -8,6 +8,23 @@
       <chaos/hardware/amd>
       <chaos/hardware/fingerprint>
       <chaos/hardware/sensors>
+      <chaos/hardware/framework/wifi-quirks>
     ];
+  };
+
+  chaos.hardware.provides.framework.provides.wifi-quirks = {
+    nixos = {
+      networking = {
+        networkmanager.wifi = {
+          powersave = false;
+        };
+      };
+
+      # Fix for mt7925e regulatory domain issues
+      # Forces the WiFi card to use DE (Germany) regulatory domain
+      boot.extraModprobeConfig = ''
+        options cfg80211 ieee80211_regdom=DE
+      '';
+    };
   };
 }
