adb0b8d999a24eae4ab04afaa81f465da50edb7a
Fabian Sabau <git@fbsb.dev>
2026-08-24 21:20:39 +0200

    feat(ghostty): update keybindings and theme settings for improved usability

--- modules/aspects/ghostty.nix
+++ modules/aspects/ghostty.nix
@@ -7,19 +7,25 @@
         package = if pkgs.stdenv.hostPlatform.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
         # clearDefaultKeybinds = true;
         settings = {
-          keybind = [
-            "alt+t=new_tab"
-            "ctrl+shift+x=toggle_split_zoom"
-            "alt+s=new_split:down"
-            "alt+shift+s=new_split:right"
-            "ctrl+page_up=next_tab"
-            "ctrl+page_down=previous_tab"
-            "alt+up=goto_split:up"
-            "alt+left=goto_split:left"
-            "alt+right=goto_split:right"
-            "alt+down=goto_split:down"
-          ];
-          theme = "light:Ayu Light,dark:Ayu";
+          keybind =
+            let
+              isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
+              key = if isDarwin then "cmd" else "alt";
+            in
+            [
+              "${key}+t=new_tab"
+              "ctrl+shift+x=toggle_split_zoom"
+              "${key}+s=new_split:down"
+              "${key}+shift+s=new_split:right"
+              "ctrl+page_up=next_tab"
+              "ctrl+page_down=previous_tab"
+              "${key}+up=goto_split:up"
+              "${key}+left=goto_split:left"
+              "${key}+right=goto_split:right"
+              "${key}+down=goto_split:down"
+            ];
+          theme = "light:Monokai Pro Light,dark:Monokai Pro";
+          shell-integration-features = "cursor,no-sudo,title,ssh-env,ssh-terminfo,path";
           tab-inherit-working-directory = false;
           window-inherit-working-directory = false;
         };
