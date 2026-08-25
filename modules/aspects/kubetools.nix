efd0debd7df1000702e103eb855f062dc429031b
Fabian Sabau <git@fbsb.dev>
2026-08-25 15:22:16 +0200

    feat(kubetools): add kubernetes-helm and k alias

--- modules/aspects/kubetools.nix
+++ modules/aspects/kubetools.nix
@@ -4,10 +4,15 @@
       programs.k9s.enable = true;
       home.packages = with pkgs; [
         kubectl
+        (runCommand "kubectl-k" { } ''
+          mkdir -p $out/bin
+          ln -s ${lib.getExe kubectl} $out/bin/k
+        '')
         kubectx
         stern
         kubelogin-oidc
         fzf
+        kubernetes-helm
       ];
     };
   };
